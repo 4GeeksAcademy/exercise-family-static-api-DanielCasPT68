877eac683c07e8123df59ce36d73ae84fcdfc934
Daniel Castillo <116021248+CodingwCastle@users.noreply.github.com>
2026-06-20 17:19:07 +0000

    Exercise done!

--- src/app.py
+++ src/app.py
@@ -10,36 +10,61 @@ from datastructures import FamilyStructure
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-CORS(app)
 
-# Create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+jackson_family.add_member({
+    "first_name": "John",
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+})
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
+jackson_family.add_member({
+    "first_name": "Jane",
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+})
 
-
-# Generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+jackson_family.add_member({
+    "first_name": "Jimmy",
+    "age": 5,
+    "lucky_numbers": [1]
+})
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+def get_all_members():
+    return jsonify(jackson_family.get_all_members()), 200
 
 
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
 
-# This only runs if `$ python src/app.py` is executed
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+
+    return jsonify(member), 200
+
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    request_body = request.json
+
+    new_member = jackson_family.add_member(request_body)
+
+    return jsonify(new_member), 200
+
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    was_deleted = jackson_family.delete_member(member_id)
+
+    if was_deleted:
+        return jsonify({"done": True}), 200
+
+    return jsonify({"error": "Member not found"}), 404
+
+
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(host='0.0.0.0', port=3000)
